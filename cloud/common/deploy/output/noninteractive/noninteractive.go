1b7d511ecec78c93f81c0378200f42e2dd6cac9f
Tim Holm <tim.holm@nitric.io>
2023-10-04 11:22:23 +1100

    remove logging prefix for now.

--- cloud/common/deploy/output/noninteractive/noninteractive.go
+++ cloud/common/deploy/output/noninteractive/noninteractive.go
@@ -14,13 +14,14 @@ type NonInteractiveOutput struct {
 func (n *NonInteractiveOutput) Write(bytes []byte) (int, error) {
 	msg := string(bytes)
 	msg, _ = strings.CutSuffix(msg, "\n")
-	n.logger.Debug(msg)
+	n.logger.Print(msg)
 	return len(bytes), nil
 }
 
 func NewNonInterativeOutput(output io.Writer) io.Writer {
 	logger := log.New(output)
-	logger.SetLevel(log.DebugLevel)
+	logger.SetPrefix("")
+
 	return &NonInteractiveOutput{
 		logger: logger,
 	}
